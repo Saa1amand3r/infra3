5697204f0aa56f3fba0856a80098bf38000e6831
erik gerb <erik.gerb@gmail.com>
2025-05-18 19:31:22 +0200

    oh no bitchini api'ini

--- backend/src/main/java/be/kdg/programming5/backend/controller/QuotesController.java
+++ backend/src/main/java/be/kdg/programming5/backend/controller/QuotesController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/quotes")
+@RequestMapping("/api/quotes")
 @Slf4j
 public class QuotesController {
     private final QuoteService quoteService;
